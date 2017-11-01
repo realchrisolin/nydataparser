cfb91cfb8c59a46e6209a644cc955ec0b922994a
Chris Olin <chris.olin@cloudcheckr.com>
2017-11-01 12:56:27 -0400

    drill down results

--- test.rb
+++ test.rb
@@ -4,9 +4,9 @@ require 'soda/client'
 
 client = SODA::Client.new({:domain => "data.ny.gov", :app_token => ENV["NYDATAAPITOKEN"]})
 
-results = client.get("gvhy-7eph", {"$limit" => 50000, :county => "Monroe"})
+results = client.get("gvhy-7eph", {"$limit" => 50000, "$order" => "create_time", :county => "Monroe", :facility_name => "NY 96", :event_type => "multi vehicle accident"})
 
 puts "Got #{results.count} results. Dumping first results:"
-results.first.each do |key, value|
+results.each do |key, value|
   puts "#{key}: #{value}"
 end
