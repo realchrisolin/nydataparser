1bec35f49a01fa31e53d954a4523e2a475b38a49
Chris Olin <chris.olin@cloudcheckr.com>
2017-11-01 13:47:43 -0400

    we don't care about multi vehicle accidents anymore

--- test.rb
+++ test.rb
@@ -4,9 +4,12 @@ require 'soda/client'
 
 client = SODA::Client.new({:domain => "data.ny.gov", :app_token => ENV["NYDATAAPITOKEN"]})
 
-results = client.get("gvhy-7eph", {"$limit" => 50000, "$order" => "create_time", :county => "Monroe", :facility_name => "NY 96", :event_type => "multi vehicle accident"})
+results = client.get("gvhy-7eph", {"$limit" => 50000, "$order" => "create_time", :county => "Monroe", :facility_name => "NY 96", :event_type => "accident"})
 
-puts "Got #{results.count} results. Dumping first results:"
+puts "Got #{results.count} results."
+=begin
+puts "Got #{results.count} results. Dumping results:"
 results.each do |key, value|
-  puts "#{key}: #{value}"
+	puts "#{key}: #{value}"
 end
+=end
