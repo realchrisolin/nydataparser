eddb8a28f6a1880ce87560f8e587914ce81174f0
Chris Olin <chris@chrisolin.com>
2017-11-03 12:06:11 -0400

    order by date and only events in Monroe County

--- test.rb
+++ test.rb
@@ -5,7 +5,8 @@ require 'soda/client'
 client = SODA::Client.new({:domain => "data.ny.gov", :app_token => ENV["NYDATAAPITOKEN"]})
 
 # populate results with https://data.ny.gov/Transportation/Motor-Vehicle-Crashes-Case-Information-Three-Year-/e8ky-4vqe dataset
-results = client.get("w9dr-rq6v", {"$limit" => 50000, :"County Name" => "MONROE"})
+results = client.get("w9dr-rq6v", {"$limit" => 50000, "$order" => "date", :"County Name" => "MONROE"})
+#results = client.get("w9dr-rq6v", {"$limit" => 500000, "$order" => "date"})
 
 #puts "Got #{results.count} results."
 puts "Got #{results.count} results. Dumping results:"
