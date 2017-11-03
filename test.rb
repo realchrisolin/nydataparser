593bcd9da7242a089fc90ab834891dfa94db22eb
Chris Olin <chris@chrisolin.com>
2017-11-03 11:49:46 -0400

    comment results count, uncomment do loop, show first result

--- test.rb
+++ test.rb
@@ -7,10 +7,8 @@ client = SODA::Client.new({:domain => "data.ny.gov", :app_token => ENV["NYDATAAP
 # populate results with https://data.ny.gov/Transportation/Motor-Vehicle-Crashes-Case-Information-Three-Year-/e8ky-4vqe dataset
 results = client.get("w9dr-rq6v", {"$limit" => 50000, :"County Name" => "MONROE"})
 
-puts "Got #{results.count} results."
-=begin
+#puts "Got #{results.count} results."
 puts "Got #{results.count} results. Dumping results:"
-results.each do |key, value|
+results.first.each do |key, value|
 	puts "#{key}: #{value}"
 end
-=end
